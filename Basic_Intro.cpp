f6b47a063a59d35a5ec6621549398e29289d39d7
laxmi_narsimha_reddy_nalla <126189357+laxminarsimha01@users.noreply.github.com>
2025-07-17 18:45:29 +0530

    Uploaded via local Repo
    In this session, I have learnt about Basics of CPP which includes:
    1. Input and Output methods
    2. Variables, Data Types
    3. Data Type Overflow

---
+++ Basic_Intro.cpp
@@ -0,0 +1,48 @@
+//MyFirstApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    // Part-1 Output messages to the console
+    cout << "Hello World!" << endl;
+    cout << "This is my first C++ application.\n";
+    cout << "Myself, Narsimha Reddy Learning c++ Programming." << endl;
+    
+
+    // Part-2
+    // Data Types
+    // int, float, double, char, string, bool
+    // Variables
+    double annualSalary;
+    cout << "Enter your Annual Salary: ";
+    cin >>  annualSalary; // User input for annual salary
+    double monthlySalary = annualSalary / 12;
+    cout <<"Your Monthly Salary is " << monthlySalary << endl;
+    cout << "In 10 Years, your will earn " <<(annualSalary * 10) << endl;
+
+    // char
+    char character = 'A';
+
+    // Give your Variable a valuable name for easy understanding
+    // Don't use single variable names or dont use special characters
+    // Variable names cannot begin with numbers
+
+    // sizeOf operator for data types to know the size in bytes
+    cout << "Size of int: " << sizeof(int) << " bytes" << " and max values is "<< INT_MAX<< " min value is " << INT_MIN << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of bool: " << sizeof(bool) << " bytes" << endl;
+    cout << "Size of string: " << sizeof(string) << " bytes" << endl;  
+    
+    
+    // Data Type Overflow
+    int intMax = INT_MAX;
+    cout << "Max Value is : " << INT_MAX << endl;
+    cout << "Max Value + 1 is : " << intMax+1 << endl; // This will cause overflow
+    cout << "Min Value is : " << INT_MIN << endl;
+    cout << "Min Value - 1 is : " << intMax-1 << endl; // This will cause underflow
+
+}
+//Alt+Ctrl+N to Run the Code
